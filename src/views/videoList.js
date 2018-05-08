4951df1d9dd841706ace234f7a39711ceccb0945
rtsuzaki <rtsuzaki10@gmail.com>
2018-05-07 21:57:36 -0700

    Rendered app view and five different video entries

--- src/views/videoList.js
+++ src/views/videoList.js
@@ -10,17 +10,11 @@ var VideoListView = Backbone.View.extend({
   render: function() {
     this.$el.children().detach();
     this.$el.html(this.template());
-    // this.collection.models.forEach(this.renderVideo, this);
     this.collection.models.forEach(function(video) {
       new VideoListEntryView({model: video});
-    })
+    });
     return this;
   },
-  
-  // renderVideo: function(video) {
-  //   var VideoListEntryView = new VideoListEntryView({model: video});
-  //   this.$el.append(VideoListEntryView.render());
-  // }
 
   template: templateURL('src/templates/videoList.html')
 
